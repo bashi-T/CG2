833f961c1b02e8794c939b06be883fefe0c65fe6
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2024-07-22 12:49:40 +0900

    ittan

--- project/HLSL/SkyBox.PS.hlsl
+++ project/HLSL/SkyBox.PS.hlsl
@@ -4,7 +4,7 @@ struct Material
 {
 float32_t4 color;
 };
-ConstantBuffer<Material> gMaterial:register(b0);
+ConstantBuffer<Material> gMaterial : register(b0);
 
 TextureCube<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
@@ -18,6 +18,6 @@ PixelShaderOutput main(VertexShaderOutput input)
 {
   PixelShaderOutput output;
   float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-  output.color = gMaterial.color * textureColor;
+  output.color = textureColor * gMaterial.color;
   return output;
 }
--- project/HLSL/SkyBox.VS.hlsl
+++ project/HLSL/SkyBox.VS.hlsl
@@ -16,7 +16,7 @@ struct VertexShaderInput
 VertexShaderOutput  main(VertexShaderInput input)
 {
   VertexShaderOutput output;
-  output.position = mul(input.position, gTransformationMatrix.WVP).xyzw;
+  output.position = mul(input.position, gTransformationMatrix.WVP).xyww;
   output.texcoord = input.position.xyz;
   return output;
 }
